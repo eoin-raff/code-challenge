b82f6c2d3a7bc892ab73ddc5fac3739ef6f713fc
Eoin Rafferty <eoinrafferty@gmail.com>
2024-11-10 15:52:12 +0100

    user.hasPiece() working as expectedcd

--- __tests__/userCanBuildSet.test.ts
+++ __tests__/userCanBuildSet.test.ts
@@ -4,16 +4,55 @@ import User from "../src/classes/user"
 import Set from "../src/classes/set"
 
 describe('userCanBuildSet', () => {
+    test('returns false if the user has no bricks', () => {
+        const testUser = new User({ ...user, brickCount: 0 })
+        const testSet = new Set({ ...set, totalPieces: 10 })
+        const result = userCanBuildSet(testUser, testSet)
+        expect(result).toBe(false)
+    })
     test('returns false if user has fewer pieces than the set requires', () => {
         const testUser = new User({ ...user, brickCount: 5 })
         const testSet = new Set({ ...set, totalPieces: 10 })
         const result = userCanBuildSet(testUser, testSet)
         expect(result).toBe(false)
     })
-    test('returns false if the user has no bricks', ()=>{
-        expect(true).toBe(false)
-    })
-    test('returns false if the user is missing a required brick', ()=>{
-        expect(true).toBe(false)
+    test('returns false if the user is missing a required brick', () => {
+        const testUser = new User({
+            ...user, brickCount: 10, collection: [{
+                "pieceId": "3710",
+                "variants": [
+                    {
+                        "color": "155",
+                        "count": 9
+                    },
+                    {
+                        "color": "5",
+                        "count": 1
+                    },
+                ]
+            }]
+        })
+        const testSet = new Set({
+            ...set, totalPieces: 5, "pieces": [
+                {
+                    "part": {
+                        "designID": "3710",
+                        "material": 155,
+                        "partType": "rigid"
+                    },
+                    "quantity": 4
+                },
+                {
+                    "part": {
+                        "designID": "112345",
+                        "material": 155,
+                        "partType": "rigid"
+                    },
+                    "quantity": 1
+                },
+            ]
+        })
+        const result = userCanBuildSet(testUser, testSet)
+        expect(result).toBe(false)
     })
 })
